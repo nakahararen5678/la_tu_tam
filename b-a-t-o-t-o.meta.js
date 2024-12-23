7e776cfc94d64491a4f5db89ee109bb937fc26be
nakahararen5678 <nakahararen5678@github.com>
2024-12-23 13:23:41 +0700

    init

---
+++ b-a-t-o-t-o.meta.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         b-a-t-o-t-o
+// @namespace    b-a-t-o-t-o.user.js
+// @version      2024.12.23
+// @description  down b-a-t-o-t-o
+// @author       You
+// @match        https://fto.to/chapter/*
+// @match        https://jto.to/chapter/*
+// @match        https://hto.to/chapter/*
+// @match        https://xbato.com/chapter/*
+// @match        https://xbato.net/chapter/*
+// @match        https://xbato.org/chapter/*
+// @match        https://zbato.com/chapter/*
+// @match        https://zbato.net/chapter/*
+// @match        https://zbato.org/chapter/*
+// @match        https://readtoto.com/chapter/*
+// @match        https://readtoto.net/chapter/*
+// @match        https://readtoto.org/chapter/*
+// @match        https://batocomic.com/chapter/*
+// @match        https://batocomic.net/chapter/*
+// @match        https://batocomic.org/chapter/*
+// @match        https://batotoo.com/chapter/*
+// @match        https://batotwo.com/chapter/*
+// @match        https://comiko.net/chapter/*
+// @match        https://comiko.org/chapter/*
+// @match        https://battwo.com/chapter/*
+// @match        https://dto.to/chapter/*
+// @match        https://mto.to/chapter/*
+// @match        https://wto.to/chapter/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=batotoo.com
+// @require      https://cdn.jsdelivr.net/npm/@zip.js/zip.js@2.7.53/dist/zip.min.js
+// @grant        none
+// @downloadURL  https://github.com/nakahararen5678/la_tu_tam/raw/main/b-a-t-o-t-o.user.js
+// @updateURL    https://github.com/nakahararen5678/la_tu_tam/raw/main/b-a-t-o-t-o.meta.js
+// ==/UserScript==
